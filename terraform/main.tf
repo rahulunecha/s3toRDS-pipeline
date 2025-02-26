a6e0d07ea1bdd5b78865f35fc94c07d61ceadc5a
Ra}{ul Unecha <runecha08@gmail.com>
2025-02-26 21:49:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,23 +3,24 @@ resource "aws_s3_bucket" "data_bucket" {
 }
 
 resource "aws_db_instance" "rds_instance" {
-  engine = "mysql"
-  instance_class = "db.t3.micro"
+  engine            = "mysql"
+  instance_class    = "db.t3.micro"
   allocated_storage = 20
-  username = "admin"
-  password = "password123"
+  username          = "admin"
+  password          = "password123"
 }
 
 resource "aws_lambda_function" "data_handler" {
   function_name = "dataHandler"
-  image_uri = aws_ecr_repository.data_pipeline.repository_url
-  package_type = "Image"
-  role = aws_iam_role.lambda_role.arn
+  image_uri     = aws_ecr_repository.data_pipeline.repository_url
+  package_type  = "Image"
+  role          = aws_iam_role.lambda_role.arn
 }
 
 resource "aws_ecr_repository" "data_pipeline" {
   name = "data-pipeline"
 }
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-execution-role"
 
@@ -35,10 +36,4 @@ resource "aws_iam_role" "lambda_role" {
       },
     ]
   })
-}
-resource "aws_lambda_function" "data_handler" {
-  function_name = "data-handler"
-  image_uri     = aws_ecr_repository.data_pipeline.repository_url
-  role          = aws_iam_role.lambda_role.arn
-  package_type  = "Image"
 }
