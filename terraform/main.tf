6965a9101bf3404d62b58c193fc4283c974b693d
Ra}{ul Unecha <runecha08@gmail.com>
2025-02-26 21:45:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,29 @@ resource "aws_lambda_function" "data_handler" {
   package_type = "Image"
   role = aws_iam_role.lambda_role.arn
 }
+
+resource "aws_ecr_repository" "data_pipeline" {
+  name = "data-pipeline"
+}
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_lambda_function" "data_handler" {
+  function_name = "data-handler"
+  image_uri     = aws_ecr_repository.data_pipeline.repository_url
+  role          = aws_iam_role.lambda_role.arn
+  package_type  = "Image"
+}
